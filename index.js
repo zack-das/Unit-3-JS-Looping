86d9c5acb9005ed04db4798a854b5acf35591b25
zack-das <izackdas@gmail.com>
2025-07-21 08:16:59 +0300

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards;
+}
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
